3785b722aef7ac4a607cc9f438b4c80ab7f929ac
Gotcha7770 <gbakanov@inbox.ru>
2021-04-25 16:41:32 +0300

    add modules to execute process and work with regex

---
+++ PoshGitModule/ExecuteProcess.fs
@@ -0,0 +1,36 @@
+﻿module ExecuteProcess
+
+open System.Collections.Generic
+open System.Diagnostics
+
+type ProcessResult =
+    { exitCode: int
+      stdout: List<string>
+      stderr: List<string> }
+
+let private executeFromInfo (procInfo: ProcessStartInfo) =
+
+    let proc = Process.Start(procInfo)
+
+    let output = List<string>()
+    proc.OutputDataReceived.Add(fun x -> output.Add(x.Data))
+    let error = List<string>()
+    proc.ErrorDataReceived.Add(fun x -> error.Add(x.Data))
+
+    proc.BeginErrorReadLine()
+    proc.BeginOutputReadLine()
+    proc.WaitForExit()
+
+    { exitCode = proc.ExitCode
+      stdout = output
+      stderr = error }
+
+let executeProcess (name, args) =
+    let procInfo = ProcessStartInfo(name, args)
+
+    procInfo.UseShellExecute <- false
+    procInfo.RedirectStandardOutput <- true
+    procInfo.RedirectStandardError <- true
+    procInfo.CreateNoWindow <- true
+
+    executeFromInfo procInfo
---
+++ PoshGitModule/RegularExpressions.fs
@@ -0,0 +1,13 @@
+﻿module RegularExpressions
+
+open System.Text.RegularExpressions
+
+let matchSample expr text =
+    let r = Regex(expr)
+    
+    r.Match text
+
+let isMatch expr text =
+    let r = matchSample expr text
+    
+    r.Success
