75d449325345bf52a831959c4d79c8cd7739cb04
Gotcha7770 <gbakanov@inbox.ru>
2021-04-25 16:31:37 +0300

    add solution

---
+++ PoshGitModule/Clear-Branches.fs
@@ -0,0 +1,27 @@
+﻿namespace PoshGitModule
+
+open System.Management.Automation
+open ExecuteProcess
+open RegularExpressions
+
+[<Cmdlet("Clear", "Branches")>]
+type ClearBranchesCommand() =
+    inherit PSCmdlet()
+
+    let executeGit args = executeProcess ("git", args)
+    let expr = ": gone]" //branch is gone from origin marker
+    
+    override x.EndProcessing() =
+        let result = executeGit "branch -vv"
+        let branches =
+            result.stdout
+            //result.stdout.Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+            |> Seq.filter (fun x -> isMatch expr x)
+            |> Seq.map (fun x -> x.Trim().Split(" ").[0])
+            //|> Seq.iter (fun x -> executeGit $"branch -d {x}" |> ignore)
+            
+        for branch in branches do
+            executeGit $"branch -d {branch}" |> ignore
+            printfn $"Branch {branch} has been deleted!"
+            
+        base.EndProcessing()
