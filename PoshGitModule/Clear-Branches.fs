66129153d6ea1b1744a34caab8d2af897fdb8af7
Gotcha7770 <gbakanov@inbox.ru>
2021-04-25 17:26:48 +0300

    cleanup

--- PoshGitModule/Clear-Branches.fs
+++ PoshGitModule/Clear-Branches.fs
@@ -15,13 +15,11 @@ type ClearBranchesCommand() =
         let result = executeGit "branch -vv"
         let branches =
             result.stdout
-            //result.stdout.Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
             |> Seq.filter (fun x -> isMatch expr x)
             |> Seq.map (fun x -> x.Trim().Split(" ").[0])
-            //|> Seq.iter (fun x -> executeGit $"branch -d {x}" |> ignore)
             
         for branch in branches do
             executeGit $"branch -d {branch}" |> ignore
-            printfn $"Branch {branch} has been deleted!"
+            x.WriteObject($"Branch {branch} has been deleted!")
             
         base.EndProcessing()
